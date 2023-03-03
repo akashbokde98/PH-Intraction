8b81672c0a0dac09ba6b2a63422056b10bcce243
akashbokde98 <akashbokde98@gmail.com>
2023-03-03 18:23:17 +0530

    Added:Abstrction & Encapsulation Code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AbstractClass
+{
+    abstract class EmpName
+    {
+
+        protected string name;
+        public abstract string Name
+        {
+            get;
+            set;
+        }
+    }
+    class EmpFirstName : EmpName
+    {
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an object of Dog class
+            EmpFirstName obj = new EmpFirstName();
+            obj.Name = "Akash";
+            Console.WriteLine("Name: " + obj.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
